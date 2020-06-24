5d5beb04100a78d6586741c475c2e343fa44011b
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2020-06-24 22:15:45 +0200

    Update reviews_controller.rb

--- app/controllers/spree/admin/reviews_controller.rb
+++ app/controllers/spree/admin/reviews_controller.rb
@@ -17,17 +17,18 @@ class Spree::Admin::ReviewsController < Spree::Admin::ResourceController
   
   def approve
     review = Spree::Review.find(params[:id])
-    if review.update_attribute(:approved, true)
-      #curr_product = review.product
-      #reviews = curr_product.reload.reviews.approved
+    review.update_attribute!(approved: true)
+#     if 
+#       #curr_product = review.product
+#       #reviews = curr_product.reload.reviews.approved
 
-      #curr_product.update!(reviews_count: reviews.count)
-      #curr_product.update!(avg_rating: reviews.sum(:rating).to_f / reviews.count)
+#       #curr_product.update!(reviews_count: reviews.count)
+#       #curr_product.update!(avg_rating: reviews.sum(:rating).to_f / reviews.count)
         
-      flash[:notice] = Spree.t(:info_approve_review)
-    else
-      flash[:error] = review.errors
-    end
+#       flash[:notice] = Spree.t(:info_approve_review)
+#     else
+#       flash[:error] = review.errors
+#     end
     redirect_to admin_reviews_path
   end
 
