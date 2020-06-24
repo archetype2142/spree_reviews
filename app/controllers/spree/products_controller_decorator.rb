bd25e02339f47762bfbc9883a8d57d3048bdba52
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2020-06-24 21:21:39 +0200

    Update products_controller_decorator.rb

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,11 +1,13 @@
-module Spree::ProductsControllerDecorator
-  helper Spree::ReviewsHelper
+module Spree
+  class ProductsControllerDecorator
+    helper Spree::ReviewsHelper
 
-  reviews_fields = [:avg_rating, :reviews_count]
-  reviews_fields.each { |attrib| Spree::PermittedAttributes.product_attributes << attrib }
+    reviews_fields = [:avg_rating, :reviews_count]
+    reviews_fields.each { |attrib| Spree::PermittedAttributes.product_attributes << attrib }
 
-  Spree::Api::ApiHelpers.class_eval do
-    reviews_fields.each { |attrib| class_variable_set(:@@product_attributes, class_variable_get(:@@product_attributes).push(attrib)) }
+    Spree::Api::ApiHelpers.class_eval do
+      reviews_fields.each { |attrib| class_variable_set(:@@product_attributes, class_variable_get(:@@product_attributes).push(attrib)) }
+    end
   end
 end
 
