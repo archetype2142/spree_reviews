379a6478c602494c77e93326b2477d36332f9237
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2020-06-24 21:21:15 +0200

    Update product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,20 +1,22 @@
-class Spree::ProductDecorator
-  def self.prepended(base)
-    base.has_many :reviews
-  end
-
-  def stars
-    avg_rating.try(:round) || 0
-  end
-
-  def recalculate_rating
-    self.reviews_count = reviews.reload.approved.count
-    if reviews_count > 0
-      self.avg_rating = reviews.approved.sum(:rating).to_f / reviews_count
-    else
-      self.avg_rating = 0
+module Spree
+  class ProductDecorator
+    def self.prepended(base)
+      base.has_many :reviews
     end
-    save!
+
+    def stars
+      avg_rating.try(:round) || 0
+    end
+
+    def recalculate_rating
+      self.reviews_count = reviews.reload.approved.count
+      if reviews_count > 0
+        self.avg_rating = reviews.approved.sum(:rating).to_f / reviews_count
+      else
+        self.avg_rating = 0
+      end
+      save!
+    end
   end
 end
 
