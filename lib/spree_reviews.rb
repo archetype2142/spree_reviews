d2c5f038748fdc4f67bd4a538b9c642eab55fb16
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2020-06-24 18:20:40 +0200

    Update spree_reviews.rb

--- lib/spree_reviews.rb
+++ lib/spree_reviews.rb
@@ -4,14 +4,6 @@ require 'spree_reviews/version'
 require 'spree_extension'
 require 'coffee_script'
 require 'sass/rails'
-require "zeitwerk"
-loader = Zeitwerk::Loader.for_gem
-
-kernel_ext = "#{__dir__}/lib"
-models_ext = "#{__dir__}/app/models/spree"
-loader.ignore(kernel_ext)
-loader.ignore(models_ext)
-loader.setup
 
 module Spree
   module Reviews    
