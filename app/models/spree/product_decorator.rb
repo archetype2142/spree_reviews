ee698fddaa0321706fa299650b01ed3c3739a947
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2020-06-24 18:57:09 +0200

    Update product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -10,11 +10,11 @@ module Spree
     end
 
     def recalculate_rating
-      self[:reviews_count] = reviews.reload.approved.count
+      self.reviews_count = reviews.reload.approved.count
       if reviews_count > 0
-        self[:avg_rating] = reviews.approved.sum(:rating).to_f / reviews_count
+        self.avg_rating = reviews.approved.sum(:rating).to_f / reviews_count
       else
-        self[:avg_rating] = 0
+        self.avg_rating = 0
       end
       save!
     end
